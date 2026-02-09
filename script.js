82d7bcc781950e247ddbebbb8fb62f915d66f329
1219624898-spec <1219624898@qq.com>
2026-02-09 22:53:55 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ function openViewer(modelName) {
     const viewer = document.getElementById("modelViewer");
     viewer.src = "models/" + modelName;
 
+    viewer.style.setProperty("--poster-color", "#87CEFA"); // 天蓝色
+
     document.getElementById("viewerModal").style.display = "block";
 }
 
